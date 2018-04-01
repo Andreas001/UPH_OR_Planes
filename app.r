ba806012b685f132707323e1f58aabdc608608b1
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-01 18:31:30 +0700

    Update and rename server.r to app.r

---
+++ app.r
@@ -0,0 +1,30 @@
+#install necessary packages in the console
+#install.packages("shiny")
+#install.packages("shinydashboard")
+#install.packages("leaflet") serves to display rendered map in the UI
+#install.packages("jsonlite")
+
+#load necessary packages
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(jsonlite)
+
+#categorize
+airPlanes = read.csv(url("https://raw.githubusercontent.com/Andreas001/UPH_RO_Case1/master/AircraftList1April1800.csv"))
+
+#Server
+shinyServer(function(input, output) 
+{
+  output$mymap <- renderLeaflet({
+    leaflet(data = airPlanes[1:input$planeCount,]) %>% addTiles() %>%
+      addMarkers(~long, ~lat, popup = ~as.character(model), label=~as.character(airLines))
+})
+})
+
+#UI
+shinyUI(fluidPage(
+  dashboardHeader(title="Airplane tracker (1 April 16:00 pm)"),
+  sliderInput("planeCount","Amount of planes",10,10,20),
+  leafletOutput("mymap")
+ ))
